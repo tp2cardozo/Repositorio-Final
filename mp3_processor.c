a8403e74beb0a3fdac9de1e63d79e70b96a458cd
Ivan <ivanrubin10@gmail.com>
2018-06-25 10:01:56 -0300

    procesor

--- mp3_processor.c
+++ mp3_processor.c
@@ -18,7 +18,7 @@ status_t process_mp3_data(setup_t * setup, FILE * fi)
 	if((st = ADT_Vector_new(&vector)) != OK)
 		return st;
 
-	if((st = mp3_reader(fi, header)) != OK)
+	if((st = get_mp3_header(fi, header)) != OK)
 		return st;
 
 
@@ -36,51 +36,14 @@ status_t get_mp3_header(FILE * fi, char header[])
         return ERROR_INVALID_MP3_FILE;
 	 
 
-    fseek(fi, 0, SEEK_END);						/* manda el puntero al final del archivo 	*/
+    if ((fseek(fi, 0, SEEK_END)) != OK)					/* manda el puntero al final del archivo 	*/
+    	return ERROR_INVALID_MP3_FILE;
 
     length=ftell(fi);							/* da la distancia al comienzo 				*/
 
-    fseek(fi,length-MP3_HEADER_SIZE,SEEK_SET);	/* se para en el header MP3 				*/
+    if((fseek(fi,length-MP3_HEADER_SIZE,SEEK_SET)) != OK)	/* se para en el header MP3 				*/
+    	return ERROR_INVALID_MP3_FILE;
 
     fread(header,sizeof(char),MP3_HEADER_SIZE,fi);
 
-    show_mp3_header(header,stdout);
-
-}
-
-status_t show_mp3_header (const char header[], FILE * fo)
-{
-    char buf[MP3_HEADER_SIZE];
-
-    if (header == NULL)
-       return ERROR_NULL_POINTER;
-
-    memcpy(buf,header+LEXEM_START_TAG,LEXEM_SPAN_TAG);
-    buf[LEXEM_SPAN_TAG] = '\0';
-    
-
-    memcpy(buf,header+LEXEM_START_TITLE,LEXEM_SPAN_TITLE);
-    buf[LEXEM_SPAN_TITLE] = '\0';
-   
-
-    memcpy(buf,header+LEXEM_START_ARTIST,LEXEM_SPAN_ARTIST);
-    buf[LEXEM_SPAN_ARTIST] = '\0';
-
-
-    memcpy(buf,header+LEXEM_START_ALBUM,LEXEM_SPAN_ALBUM);
-    buf[LEXEM_SPAN_ALBUM] = '\0';
-
-
-    memcpy(buf,header+LEXEM_START_YEAR,LEXEM_SPAN_YEAR);
-    buf[LEXEM_SPAN_YEAR] = '\0';
-
-
-    memcpy(buf,header+LEXEM_START_COMMENT,LEXEM_SPAN_COMMENT);
-    buf[LEXEM_SPAN_COMMENT] = '\0';
-
-
-    memcpy(buf,header+LEXEM_START_GENRE,LEXEM_SPAN_GENRE);
-    buf[LEXEM_SPAN_GENRE] = '\0';
-
-    return OK;
 }
