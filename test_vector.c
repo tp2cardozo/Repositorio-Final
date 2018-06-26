981e4116195d00987a1ad5b0248a5192af932d43
Manuel Maceira <manuelmaceira.m@gmail.com>
2018-06-26 18:04:59 -0300

    testing compare

--- test_vector.c
+++ test_vector.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[]) {
 	status_t st;
 	bool_t vacio;
 	ADT_Vector_t * test;
-	ADT_track_t * track;
+	ADT_track_t *track1, *track2;
 
 	size_t length;
 	FILE * f;
 	char header[MP3_HEADER_SIZE];
 
-	if ((f = fopen("track.mp3","rb")) == NULL) {
+	if ((f = fopen("track1.mp3","rb")) == NULL) {
         fprintf(stderr,"%s\n","No se pudo abrir el archivo.");
         return 1;
     }
@@ -30,19 +30,48 @@ int main(int argc, char *argv[]) {
 
     fread(header,sizeof(char),MP3_HEADER_SIZE,f);
 
-	st = ADT_track_new (&track);
-	if (st != OK) {
-		fprintf(stderr, "%s\n", errors_dictionary[st]);
-		return st;
-	}
-	printf("Creamos track OK\n");
+	fclose(f);
 
-	st = ADT_track_set (header, track);
+	st = ADT_track_new (&track1);
 	if (st != OK) {
 		fprintf(stderr, "%s\n", errors_dictionary[st]);
 		return st;
 	}
-	printf("Seteamos track OK\n");
+	printf("Creamos track1 OK\n");
+
+	st = ADT_track_set (header, track1);
+	if (st != OK) {
+		fprintf(stderr, "%s\n", errors_dictionary[st]);
+		return st;
+	}
+	printf("Seteamos track1 OK\n");
+
+	if ((f = fopen("track2.mp3","rb")) == NULL) {
+        fprintf(stderr,"%s\n","No se pudo abrir el archivo.");
+        return 1;
+    }
+
+    fseek(f, 0, SEEK_END);						/* manda el puntero al final del archivo 	*/
+    length=ftell(f);							/* da la distancia al comienzo 				*/
+    fseek(f,length-MP3_HEADER_SIZE,SEEK_SET);	/* se para en el header MP3 				*/
+
+    fread(header,sizeof(char),MP3_HEADER_SIZE,f);
+
+	fclose(f);
+
+	st = ADT_track_new (&track2);
+	if (st != OK) {
+		fprintf(stderr, "%s\n", errors_dictionary[st]);
+		return st;
+	}
+	printf("Creamos track2 OK\n");
+
+	st = ADT_track_set (header, track2);
+	if (st != OK) {
+		fprintf(stderr, "%s\n", errors_dictionary[st]);
+		return st;
+	}
+	printf("Seteamos track2 OK\n");
 
 	st = ADT_Vector_new (&test);
 	if (st != OK) {
@@ -76,13 +105,21 @@ int main(int argc, char *argv[]) {
 	}
 	printf("Seteamos destructor a \"ADT_track_delete\"\n");
 
-	st = ADT_Vector_append_element(&test, track, ADT_Vector_delete);
+	st = ADT_Vector_append_element(&test, track1, ADT_Vector_delete);
 	if (st != OK) {
 		fprintf(stderr, "%s\n", errors_dictionary[st]);
 		return st;
 	}
-	printf("Le damos 1 valor a el array\n");
+	printf("Le damos 1er valor a el array\n");
 	printf("title: %s\n", ((ADT_track_t *)(test->elements[0]))->title);
+
+	st = ADT_Vector_append_element(&test, track2, ADT_Vector_delete);
+	if (st != OK) {
+		fprintf(stderr, "%s\n", errors_dictionary[st]);
+		return st;
+	}
+	printf("Le damos 2do valor a el array\n");
+	printf("title: %s\n", ((ADT_track_t *)(test->elements[1]))->title);
 
 	st = ADT_Vector_delete (&test);
 	if (st != OK) {
@@ -90,7 +127,6 @@ int main(int argc, char *argv[]) {
 		return st;
 	}
 
-	fclose(f);
 	printf("Borramos Vector OK\n");
 	return OK;
 }
